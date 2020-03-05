10920d1e8906e269a784f9ad7ea750ca4a3b19f2
xiaodanhuang <529458979@qq.com>
2020-03-05 20:31:45 +0800

    modify vue

--- javascript/function/js/deepClone.js
+++ javascript/function/js/deepClone.js
@@ -1,6 +1,25 @@
-//待补充
-function dfsDeepClone(obj){
+//深度克隆 基本数据类型/引用数据类型
+function deepClone(obj){
     if(obj=null) return ;
     if(typeof obj=='number') return
 
+}
+
+
+function cloneUtil(target){
+    let result;
+    if(getType(target)==='object'){
+        result={}
+    }else if(getType(target)==='array'){
+        result=[]
+    }else {
+        return target
+    }
+    return result
+
+
+}
+
+function getType(target){
+    return Object.prototype.toString.call(target).slice(8,-1)
 }
